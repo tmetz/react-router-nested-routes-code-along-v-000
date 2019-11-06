b1785f52942c946983d433ebfc00057f5d393f11
Tammy Metz <tammy@tammymetz.com>
2019-11-06 04:19:44 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -7,7 +7,7 @@ import MovieShow from '../components/MovieShow';
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
-    <Route path={`${match.url}/:movieId`} component={MovieShow} />
+    <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...RouterProps } movies={movies} />} />
   </div>
 )
 
