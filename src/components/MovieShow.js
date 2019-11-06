b14d0664cd97f01b73138721e2fe99ee55fede45
Tammy Metz <tammy@tammymetz.com>
2019-11-06 04:22:26 +0000

    Done.

---
+++ src/components/MovieShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieShow = ({match, movies}) => {
+
+  return (
+    <div>
+      <h3>{ movies[,atch.params.movieId].title }</h3>
+    </div>
+  );
+}
+
+export default MovieShow;
